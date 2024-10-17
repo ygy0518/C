fb8c6cee9b56644d2e74702f6745518522562020
ygy0518 <ygy0518@gmail.com>
2024-10-17 20:11:11 +0900

    for exam2

---
+++ opensourceprogramming/week06/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var f float64
+	var i int
+	var b bool
+	var s string
+
+	a := "55"
+	println(a)
+
+	my_school_account := 5
+	println(my_school_account)
+
+	q7x1abc := 5
+	println(q7x1abc)
+
+	fmt.Printf("%f %t %s %d\n", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f <= float64(i), "\n")
+}
---
+++ opensourceprogramming/week07/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("점수 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	i, err := in.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)                // 줄바꿈등 제거, 파이썬의 strip 함수와 비슷
+	score, _ := strconv.ParseInt(i, 10, 32) // 10진 정수형(32bit)으로 변환
+
+	var grade string
+	if score >= 90 {
+		grade = "A"
+	} else {
+		grade = "BCDF"
+	}
+	fmt.Printf("%d점은 %s등급 입니다.\n", score, grade)
+}
